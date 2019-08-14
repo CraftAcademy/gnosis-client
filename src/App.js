ee57588e38bcc4578b0c4c05ec37623597135409
Daria Lykova <daria.lykova@gmail.com>
2019-08-14 18:18:15 +0200

    Creates a Components folder and a file for DisplayAllArticles component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>GNOSIS</h1>
-      </div >
-    )
-  };
+  return(
+    <div>
+      <h1>GNOSIS</h1>
+    </div >
+  )};
 }
 
 
---
+++ src/Components/DisplayAllArticles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DisplayAllArticles extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+
+}
+
+export default DisplayAllArticles
