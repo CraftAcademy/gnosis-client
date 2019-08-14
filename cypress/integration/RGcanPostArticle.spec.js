85e706e96f7ffe8d49ea67d6d75414f7f9a9f524
erka <erik.westerberg@hotmail.se>
2019-08-14 15:31:13 +0200

    implemented more steps in the specs test for rg can post

--- cypress/integration/RGcanPostArticle.spec.js
+++ cypress/integration/RGcanPostArticle.spec.js
@@ -2,4 +2,12 @@ describe('RG can post article', () => {
   before(function() {
     cy.visit('http://localhost:3001');
     cy.get('#create-article').click();
-}
+    cy.get('#post-article-form').within(() => {
+      cy.get('#Author-id'.type('JohnDoe'))
+      cy.get('#title').type('To be or not to be')
+      cy.get('#body').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+      cy.get('submit-button').click()
+    })
+    cy.contains('Post successfully created')
+  })
+})
