38444747d472be8c31f37cbb45b647d605d4b993
Daria Lykova <daria.lykova@gmail.com>
2019-08-14 18:29:44 +0200

    Creates a Modules folder and a file for getArticlesFromApi

--- src/Components/DisplayAllArticles.js
+++ src/Components/DisplayAllArticles.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { async } from 'q';
 
 class DisplayAllArticles extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      displayAllArticles: []
+    }
+  }
+  componentDidMount() {
+    this.setArticlesToState()
+  }
+
+  async setArticlesToState() {
+    let articles = await getArticlesFromApi();
+  }
   render() {
     return (
       <div>
-        
+
       </div>
     );
   }
