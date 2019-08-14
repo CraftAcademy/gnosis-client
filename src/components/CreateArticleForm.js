13cb366df6f0819e36e45ee81e5db44b29cba896
pedro_bras <pedro.c.bras@gmail.com>
2019-08-14 16:44:16 +0200

    removed react router configs and imported createarticleform component into app render

--- src/components/CreateArticleForm.js
+++ src/components/CreateArticleForm.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class CreateArticleForm extends Component {
   render() {
-    return <div />;
+    return <div>
+      Form here.
+    </div> ;
   }
 }
 
