80476cbdf671f8a91cdee75fd90e3adace615bfc
Daria Lykova <daria.lykova@gmail.com>
2019-08-14 16:43:06 +0200

    Converts the App function into a class that extends component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
-    <div>
-     <h1>GNOSIS</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>GNOSIS</h1>
+      </div >
+    )
+  };
 }
+
 
 export default App;
