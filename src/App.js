52d56fc6dee4181a9f842cea66f96fa8a4b811e3
Max Aubain <max.aubain@gmail.com>
2019-08-15 13:05:31 +0200

    Adds DisplayAllArticles component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import DisplayAllArticles from './Components/DisplayAllArticles';
 
 class App extends Component {
   render() {
   return(
     <div>
       <h1>GNOSIS</h1>
+      <DisplayAllArticles />
     </div >
   )};
 }
