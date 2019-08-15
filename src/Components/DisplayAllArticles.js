451c5209da56b18eb12212760988190ddb92e622
Max Aubain <max.aubain@gmail.com>
2019-08-15 13:09:24 +0200

    Adds axios GET call in componentDidMount() method to get articles, adds .map method to extract entries from JSON object array, adds return method to return article authors as a sanity check

--- src/Components/DisplayAllArticles.js
+++ src/Components/DisplayAllArticles.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import { async } from 'q';
+import axios from 'axios'
+//import { async } from 'q';
 
 class DisplayAllArticles extends Component {
   constructor() {
     super() 
     this.state = {
-      displayAllArticles: []
+      allArticles: []
     }
   }
   componentDidMount() {
-    this.setArticlesToState()
+    const apiUrl = "http://localhost:3000/api/v0/articles"
+    axios.get(apiUrl)
+      .then(response => {
+        this.setState({
+          allArticles: response.data
+        })
+      })
   }
 
-  async setArticlesToState() {
-    let articles = await getArticlesFromApi();
-  }
   render() {
+    const articles = this.state.allArticles
+    let articlesList
+    
+    if (articles.length > 0) {
+      articlesList = articles.map(article => {
+        return (
+          <div>
+            <p>{article.author}</p>
+          </div>
+        )
+      })
+    }
+
     return (
       <div>
-
+        { articlesList }
       </div>
     );
   }
-
 }
 
 export default DisplayAllArticles
