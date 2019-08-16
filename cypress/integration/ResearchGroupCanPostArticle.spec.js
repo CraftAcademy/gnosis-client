1711f8eb6e214d71d48d706a0c54413baf20fe76
pedro_bras <pedro.c.bras@gmail.com>
2019-08-16 20:08:04 +0200

    fixed happy path on rg can post spec file and added sad path it block

--- cypress/integration/ResearchGroupCanPostArticle.spec.js
+++ cypress/integration/ResearchGroupCanPostArticle.spec.js
@@ -1,15 +1,16 @@
 describe("RG can post article", () => {
   beforeEach(function() {
     cy.server();
+  });
+
+  it("successfuly", () => {
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/v0/articles",
-      response: "fixture:saving_article_response.json"
+      response: "fixture:successful_saving_article_response.json"
     });
     cy.visit("http://localhost:3001");
-  });
-
-  it("RG can post article", () => {
+    
     cy.get("#create-article").click();
     cy.get("#post-article-form").within(() => {
       cy.get("#author").type("John Doe");
@@ -21,4 +22,24 @@ describe("RG can post article", () => {
     });
     cy.contains("Post successfully created");
   });
+
+  it("unsuccessfuly", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v0/articles",
+      response: "fixture:unsuccessful_saving_article_response.json",
+      status: 404
+    });
+    cy.visit("http://localhost:3001");
+
+    cy.get("#create-article").click();
+    cy.get("#post-article-form").within(() => {
+      cy.get("#author").type("John Doe");
+      cy.get("#body").type(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      );
+      cy.get("#submit-button").click();
+    });
+    cy.contains("Title cant be blank");
+  })
 });
