4f09b903f6291ed06bc43dd071a5d1aed218d229
Max Aubain <max.aubain@gmail.com>
2019-08-16 11:04:10 +0200

    Refactors App.js nodes to separate DisplayAllArticles component from header

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>GNOSIS</h1>
-        <DisplayAllArticles />
+        <div>
+          <h1>GNOSIS</h1>
+        </div>
+
+        <>
+          <DisplayAllArticles />
+        </>
       </div>
     );
   }
