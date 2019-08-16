cdef32979dbe3eba74ad81e52fb1587dd112e20d
pedro_bras <pedro.c.bras@gmail.com>
2019-08-16 20:14:32 +0200

    turns saveArticle into try catch async function

--- src/modules/saveArticle.js
+++ src/modules/saveArticle.js
@@ -4,15 +4,18 @@ const apiUrl = "http://localhost:3000/api/v0";
 
 const saveArticle = async (author, title, body) => {
   const path = apiUrl + "/articles";
-
-  let response = await axios.post(path, {
-    data: {
-      author: author,
-      title: title,
-      body: body
-    }
-  });
-  return response;
+  try {
+    let response = await axios.post(path, {
+      data: {
+        author: author,
+        title: title,
+        body: body
+      }
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
 };
 
 export { saveArticle };
