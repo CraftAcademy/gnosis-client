309c40fd69d15ae9c1e0e44bd775ac1303a44a25
Max Aubain <max.aubain@gmail.com>
2019-08-17 17:40:49 +0200

    Corrects formatting in import line in ArticleTemplate.js

--- src/Components/ArticleTemplate.js
+++ src/Components/ArticleTemplate.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Container, Divider,Header, Placeholder} from 'semantic-ui-react'
+import {Container, Divider, Header, Placeholder} from 'semantic-ui-react'
 
 class ArticleTemplate extends Component {
   render() {
