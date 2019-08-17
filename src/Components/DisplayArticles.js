7189d65bcf1588cd01987380d024caac99c72b33
Max Aubain <max.aubain@gmail.com>
2019-08-18 00:21:43 +0200

    In DisplayArticles component, refactors async/await configuration for getArticles method, adds try/catch method to getArticles method adding conditional error messages to console log

--- src/Components/DisplayArticles.js
+++ src/Components/DisplayArticles.js
@@ -15,12 +15,27 @@ class DisplayArticles extends Component {
   }
 
   async getArticles() {
-    axios.get("http://localhost:3000/api/v0/articles")
-    .then(response => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v0/articles");
       this.setState({
         articles: response.data
       });
-    });
+    } catch (error) {
+      if (error.response) {
+        console.log("Server has responded to request from client with an error message");
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log("No response is received by client after request to server");
+        console.log(error.request);
+      } else {
+        console.log("The client request to the server has a configuration error");
+        console.log("Error", error.message);
+      }
+      console.log("General error message");
+      console.log(error);
+    }
   }
 
   render() {
