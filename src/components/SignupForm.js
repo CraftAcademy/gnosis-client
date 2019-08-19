dfc45a1c9e6be44a59ba62d5676ac13c88c4949c
Seth NeJame <sethnejame@gmail.com>
2019-08-19 12:18:24 +0200

    Adding name field to sign up form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 export class Signupform extends Component {
   state = {
     renderSignupForm: false,
-    renderSignupButton: true
+    renderSignupButton: true,
+    accountType: ""
   };
   render() {
     let SignupFields;
@@ -13,28 +14,47 @@ export class Signupform extends Component {
       SignupFields = (
         <form id="signup-form">
           <label />
-          <select id="method">
+          <select id="accountType">
+            Please select your account type:
             <option className="options" value="University">
               University
             </option>
+            <option className="options" value="Research Group">
+              Research Group
+            </option>
+            <option className="options" value="Reader">
+              Reader
+            </option>
           </select>
+          <label>Name</label>
+          <input id="email" />
           <label>Email</label>
           <input id="email" />
           <label>Password</label>
           <input id="password" />
           <label>Password Confirmation</label>
           <input id="password-confirmation" />
-          <input id="submit-account-button" value="signup" type="submit" />
-          <button onClick={() => this.setState({renderSignupForm: false, renderSignupButton: true  })}>Return</button>
-        </form>
+          {}
 
+          <input id="submit-account-button" value="signup" type="submit" />
+          <button
+            onClick={() =>
+              this.setState({
+                renderSignupForm: false,
+                renderSignupButton: true
+              })
+            }
+          >
+            Return
+          </button>
+        </form>
       );
     }
 
     if (this.state.renderSignupButton === true) {
       SignupButton = (
         <button
-        id="sign-up"
+          id="sign-up"
           onClick={() =>
             this.setState({ renderSignupForm: true, renderSignupButton: false })
           }
