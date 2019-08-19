9504752cfa1403b39e7f3231d33429fa834b44eb
Max Aubain <max.aubain@gmail.com>
2019-08-19 10:38:11 +0200

    Adds error handler message in DisplayArticles component if 'articles' are not available

--- src/Components/DisplayArticles.js
+++ src/Components/DisplayArticles.js
@@ -6,7 +6,8 @@ class DisplayArticles extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: [],
+      articlesAvailability: true
     };
   }
 
@@ -21,42 +22,34 @@ class DisplayArticles extends Component {
         articles: response.data
       });
     } catch (error) {
-      if (error.response) {
-        console.log(
-          "Server has responded to request from client with an error message"
-        );
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(
-          "No response is received by client after request to server"
-        );
-        console.log(error.request);
-      } else {
-        console.log(
-          "The client request to the server has a configuration error"
-        );
-        console.log("Error", error.message);
-      }
-      console.log("General error message");
-      console.log(error);
+      this.setState({
+        articlesAvailability: false
+      });
     }
   }
 
   render() {
     const articles = this.state.articles;
+    const availability = this.state.articlesAvailability;
 
-    let articlesList;
-    articlesList = articles.map(article => {
+    if (availability === true) {
+      let articlesList;
+      articlesList = articles.map(article => {
+        return (
+          <>
+            <ArticleTemplate article={article} />
+          </>
+        );
+      });
+
+      return <div className="articles">{articlesList}</div>;
+    } else if (availability === false) {
       return (
-        <>
-          <ArticleTemplate article={article} />
-        </>
+        <div>
+          <p>Articles are currently unavailable.</p>
+        </div>
       );
-    });
-
-    return <div className="articles">{articlesList}</div>;
+    }
   }
 }
 
