707af9c8c3fde906c651f13fefabba32eee2be25
Seth NeJame <sethnejame@gmail.com>
2019-08-20 08:31:21 +0200

    More refactoring/cleaning up if/else and replacing with ternary operators

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PaymentForm from "./PaymentForm";
 import { saveNewUser } from "../modules/saveNewUser";
-import { Form, Button } from 'semantic-ui-react';
+import { Container, Form, Button } from "semantic-ui-react";
 
 export class Signupform extends Component {
   state = {
@@ -13,7 +13,7 @@ export class Signupform extends Component {
     password: "",
     password_confirmation: "",
     userSaved: false,
-    errorMessage: "",
+    errorMessage: ""
   };
 
   async saveNewUserHandler(e) {
@@ -32,17 +32,12 @@ export class Signupform extends Component {
       });
     } else {
       this.setState({
-        
         errorMessage: response.data.body.message
-       
       });
-      
     }
   }
 
   render() {
-    let SignupFields;
-    let SignupButton;
     let saveUserStatus;
 
     if (this.state.userSaved === true) {
@@ -55,107 +50,108 @@ export class Signupform extends Component {
       saveUserStatus = this.state.errorMessage;
     }
 
-    if (this.state.renderSignupForm === true) {
-      SignupFields = (
-        <Form id="signup-form" onSubmit={e => this.saveNewUserHandler(e)}>
-          <Form.Field>
-          <label>Account Type</label>
-          <select
-            id="accountType"
-            value={this.state.accountType}
-            onChange={e => this.setState({ accountType: e.target.value })}
-          >
-            <option className="options" value="" disabled>
-              Choose Account. . .
-            </option>
-            <option className="options" value="University">
-              University
-            </option>
-            <option className="options" value="Research Group">
-              Research Group
-            </option>
-            <option className="options" value="Reader">
-              Reader
-            </option>
-          </select>
-          </Form.Field>
-
-          <Form.Field>
-          <label>University Name</label>
-          <input
-            id="name"
-            value={this.state.name}
-            onChange={e => this.setState({ name: e.target.value })}
-          />
-          </Form.Field>
-
-          <Form.Field>
-          <label>Email</label>
-          <input
-            id="email"
-            value={this.state.email}
-            onChange={e => this.setState({ email: e.target.value })}
-          />
-          </Form.Field>
-
-          <Form.Field>
-          <label>Password</label>
-          <input
-            id="password"
-            value={this.state.password}
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-          </Form.Field>
-
-          <Form.Field>
-          <label>Password Confirmation</label>
-          <input
-            id="password-confirmation"
-            value={this.state.password_confirmation}
-            onChange={e =>
-              this.setState({ password_confirmation: e.target.value })
-            }
-          />
-          </Form.Field>
-
-          {this.state.accountType === "University" ?  <PaymentForm /> : ''}
-
-          <Button id="submit-account-button" type="submit" >Sign Me Up!</ Button>
-            {/* After extracting this component to its own page, remove below. . .we don't need to hide the form */}
+    return (
+      <Container>
+        {this.state.renderSignupButton ? (
           <Button
+            id="sign-up"
             onClick={() =>
               this.setState({
-                renderSignupForm: false,
-                renderSignupButton: true,
-                accountType: ""
+                renderSignupForm: true,
+                renderSignupButton: false
               })
             }
           >
-            Return
-          </ Button>
-        </Form>
-      );
-    }
+            Sign Up
+          </Button>
+        ) : (
+          ""
+        )}
 
-    if (this.state.renderSignupButton === true) {
-      SignupButton = (
-        <button
-          id="sign-up"
-          onClick={() =>
-            this.setState({ renderSignupForm: true, renderSignupButton: false })
-          }
-        >
-          Sign Up
-        </button>
-      );
-    }
+        {this.state.renderSignupForm ? (
+          <Form id="signup-form" onSubmit={e => this.saveNewUserHandler(e)}>
+            <Form.Field>
+              <label>Account Type</label>
+              <select
+                id="accountType"
+                value={this.state.accountType}
+                onChange={e => this.setState({ accountType: e.target.value })}
+              >
+                <option className="options" value="" disabled>
+                  Choose Account. . .
+                </option>
+                <option className="options" value="University">
+                  University
+                </option>
+                <option className="options" value="Research Group">
+                  Research Group
+                </option>
+                <option className="options" value="Reader">
+                  Reader
+                </option>
+              </select>
+            </Form.Field>
 
-    return (
-      <div>
-        {saveUserStatus}
-        {SignupFields}
-        {SignupButton}
-      </div>
+            <Form.Field>
+              <label>University Name</label>
+              <input
+                id="name"
+                value={this.state.name}
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+            </Form.Field>
+
+            <Form.Field>
+              <label>Email</label>
+              <input
+                id="email"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </Form.Field>
+
+            <Form.Field>
+              <label>Password</label>
+              <input
+                id="password"
+                value={this.state.password}
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </Form.Field>
+
+            <Form.Field>
+              <label>Password Confirmation</label>
+              <input
+                id="password-confirmation"
+                value={this.state.password_confirmation}
+                onChange={e =>
+                  this.setState({ password_confirmation: e.target.value })
+                }
+              />
+            </Form.Field>
+
+            {this.state.accountType === "University" ? <PaymentForm /> : ""}
+
+            <Button id="submit-account-button" type="submit">
+              Sign Me Up!
+            </Button>
+            {/* After extracting this component to its own page, remove below. . .we don't need to hide the form */}
+            <Button
+              onClick={() =>
+                this.setState({
+                  renderSignupForm: false,
+                  renderSignupButton: true,
+                  accountType: ""
+                })
+              }
+            >
+              Return
+            </Button>
+          </Form>
+        ) : (
+          ""
+        )}
+      </Container>
     );
   }
 }
