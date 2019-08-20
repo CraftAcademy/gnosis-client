281f0fc86f187b079f3a71eff22a3f66caa46a67
Seth NeJame <sethnejame@gmail.com>
2019-08-20 07:53:25 +0200

    Merging dev upstream w/ universities can signup client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-<<<<<<< HEAD
 import SignupForm from "./components/SignupForm";
-=======
 import DisplayArticles from "./components/DisplayArticles";
 import { Container, Header, Divider } from "semantic-ui-react";
->>>>>>> 6323472c3596805c058ce646d5351e22156aa868
 import CreateArticleForm from "./components/CreateArticleForm";
 import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <h1>GNOSIS</h1>
-      <SignupForm />
-=======
+
     <Container>
       <Header as="h1" textAlign="center">
         GNOSIS
       </Header>
->>>>>>> 6323472c3596805c058ce646d5351e22156aa868
+      <SignupForm />
       <LoginForm />
       <CreateArticleForm />
 
