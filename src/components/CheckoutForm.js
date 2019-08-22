749d197f952544c4504e8121711314fcbec3ab43
pedro_bras <pedro.c.bras@gmail.com>
2019-08-22 21:55:29 +0200

    adds logic to submit payment function, also adds payment complete to state to render confirmation message when payment is complete

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -10,14 +10,23 @@ import { Form } from "semantic-ui-react";
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = { complete: false };
     this.submit = this.submit.bind(this);
   }
 
   async submit(ev) {
-    // User clicked submit
+    let { token } = await this.props.stripe.createToken({ name: "Name" });
+    let response = await fetch("/charge", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+
+    if (response.ok) this.setState({ complete: true });
   }
 
   render() {
+    if (this.state.complete) return <h1>Payment successful!</h1>;
     return (
       <div>
         <Form id="payment-form">
@@ -28,19 +37,19 @@ class CheckoutForm extends Component {
               Yearly - 10,000SEK/each
             </Form.Field>
           </Form.Field>
-          <Form.Field >
-          <CardNumberElement />
+          <Form.Field>
+            <CardNumberElement />
           </Form.Field>
           <Form.Field>
-          <CardExpiryElement />
+            <CardExpiryElement />
           </Form.Field>
           <Form.Field>
-          <CardCvcElement />
+            <CardCvcElement />
           </Form.Field>
           <Form.Field>
-          <button onClick={this.submit} id="submit-payment-button">
-            Proceed with Payment
-          </button>
+            <button onClick={this.submit} id="submit-payment-button">
+              Proceed with Payment
+            </button>
           </Form.Field>
         </Form>
       </div>
