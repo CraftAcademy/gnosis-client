45c3fa3532796ade90a912e018e8304f292ec491
pedro_bras <pedro.c.bras@gmail.com>
2019-08-22 13:50:18 +0200

    added redirection to payment component to feature test and adds payment form completion and submiting button

--- cypress/integration/uniCanBuySubscription.spec.js
+++ cypress/integration/uniCanBuySubscription.spec.js
@@ -11,5 +11,13 @@ describe('University can buy yearly subscription after sign up', () => {
       "password_confirmation"
     );
     cy.get("#submit-account-button").click();
+    cy.location("pathname").should("eq", "/payment");
+    cy.get("#payment-form").within(() => {
+      cy.get('input[id="card-number"]').type("4242 4242 4242 4242");
+      cy.get('input[id="card-owner"]').type("Harvard University");
+      cy.get('input[id="expiration date"]').type("10-21");
+      cy.get('input[id="CVC"]').type("123");
+    });
+    cy.get("#process-payment").click();
   });
 });
