cb533a54a054ccaa35a06f1e50da78bfc0dda11d
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-22 14:19:03 +0200

    adds tests for flash messages for happy path

--- cypress/integration/uniCanBuySubscription.spec.js
+++ cypress/integration/uniCanBuySubscription.spec.js
@@ -12,6 +12,7 @@ describe('University can buy yearly subscription after sign up', () => {
     );
     cy.get("#submit-account-button").click();
     cy.location("pathname").should("eq", "/payment");
+    cy.get('#flash').should('contain', 'Your university account successfully created!')
     cy.get("#payment-form").within(() => {
       cy.get('input[id="card-number"]').type("4242 4242 4242 4242");
       cy.get('input[id="card-owner"]').type("Harvard University");
@@ -19,5 +20,7 @@ describe('University can buy yearly subscription after sign up', () => {
       cy.get('input[id="CVC"]').type("123");
     });
     cy.get("#process-payment").click();
+    cy.location("pathname").should("eq", "/user_profile");
+    cy.get('#flash').should('contain', 'Payment was successful!')
   });
 });
