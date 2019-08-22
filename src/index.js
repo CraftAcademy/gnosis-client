69c4914812eaf420638e4598144679ae32e2c275
oliverochman <oliver.ochman@hotmail.com>
2019-08-22 09:59:52 +0200

    change API url to production

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 let development = "http://localhost:3000/api/v0";
 let production = "https://gnosis-api.herokuapp.com/api/v0";
 
-axios.defaults.baseURL = development
+axios.defaults.baseURL = production;
 
 const store = configureStore();
 
