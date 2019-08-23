8e77e4812db9153db9e8ce71fe0755148bcce53a
Max Aubain <max.aubain@gmail.com>
2019-08-23 13:18:40 +0200

    Imports packages from i18n.js to index.js for use in app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 import configureStore from "./redux/store/configureStore";
 import axios from "axios";
+import i18n from "./i18n";
 
 let development = "http://localhost:3000/api/v0";
 let production = "https://gnosis-api.herokuapp.com/api/v0";
@@ -14,15 +15,14 @@ axios.defaults.baseURL = production;
 
 const store = configureStore();
 
-window.store = store
+window.store = store;
 
 ReactDOM.render(
   <Router>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+    ,
   </Router>,
   document.getElementById("root")
 );
-
-
