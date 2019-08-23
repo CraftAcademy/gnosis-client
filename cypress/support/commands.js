f362f5e91f302cc26a9dd8eb197f7fa5b7ed9069
Seth NeJame <sethnejame@gmail.com>
2019-08-23 18:32:55 +0200

    Adding Cypress commands for cleaner testing (research group signup)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -72,3 +72,36 @@ Cypress.Commands.add(
     });
   }
 );
+
+Cypress.Commands.add(
+  "research_group_successful_signup",
+  (role, name, email, password, password_confirmation, subscription_key) => {
+    cy.visit("http://localhost:3001");
+    cy.get("#sign-up-button").click();
+    cy.get("#signup-form").within(() => {
+      cy.get('select[id="role"]').select(role);
+      cy.get('input[id="name"]').type(name);
+      cy.get('input[id="email"]').type(email);
+      cy.get('input[id="password"]').type(password);
+      cy.get('input[id="password-confirmation"]').type(password_confirmation);
+      cy.get('input[id="subscription_key"]').type(subscription_key);
+    });
+  }
+);
+
+Cypress.Commands.add(
+  "research_group_unsuccessful_signup",
+  (role, name, email, password, password_confirmation, subscription_key) => {
+    cy.visit("http://localhost:3001");
+    cy.get("#sign-up-button").click();
+    cy.get("#signup-form").within(() => {
+      cy.get('select[id="role"]').select(role);
+      cy.get('input[id="name"]').type(name);
+      cy.get('input[id="email"]').type(email);
+      cy.get('input[id="password"]').type(password);
+      cy.get('input[id="password-confirmation"]').type(password_confirmation);
+      cy.get('input[id="subscription_key"]').type(subscription_key);
+    });
+  }
+);
+
