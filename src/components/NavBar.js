eb7b96afcb1652000546cd220da9087f64b3a5c4
Max Aubain <max.aubain@gmail.com>
2019-08-23 14:06:43 +0200

    Adds non-functional languages pull down menu to top navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Container, Menu, Input } from 'semantic-ui-react';
-import '../styling/Navbar.css';
-import AlertMessage from './AlertMessage';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Container, Menu, Input } from "semantic-ui-react";
+import "../styling/Navbar.css";
+import AlertMessage from "./AlertMessage";
+import { connect } from "react-redux";
 
 class NavBar extends Component {
-  state = { activeItem: 'latest news' }
+  state = { activeItem: "latest news" };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
     let createArticleButton;
     let flashMessage;
     let loginActions;
 
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
 
     if (this.props.showFlash === true) {
-      flashMessage = <AlertMessage />
+      flashMessage = <AlertMessage />;
     }
 
     if (this.props.currentUser.attributes.role === "research_group") {
-      createArticleButton = <Menu.Item id="create-article-button" as={NavLink} to="/createarticle">Create Article</Menu.Item>
+      createArticleButton = (
+        <Menu.Item id="create-article-button" as={NavLink} to="/createarticle">
+          Create Article
+        </Menu.Item>
+      );
     }
 
     if (this.props.currentUser.isSignedIn === false) {
       loginActions = (
         <>
-          <Menu.Item as={NavLink} to="/login-form" id="login-button">Log In</Menu.Item>
-          <Menu.Item as={NavLink} to="/signup" id="sign-up-button">Sign Up</Menu.Item>
+          <Menu.Item as={NavLink} to="/login-form" id="login-button">
+            Log In
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/signup" id="sign-up-button">
+            Sign Up
+          </Menu.Item>
         </>
-      )
-
+      );
     }
     return (
       <>
@@ -41,28 +48,56 @@ class NavBar extends Component {
             <Menu.Item className="header logo" to="/">
               GNOSIS
             </Menu.Item>
-            <Menu.Item id="homebutton" as={NavLink} to="/">Home</Menu.Item>
+            <Menu.Item id="homebutton" as={NavLink} to="/">
+              Home
+            </Menu.Item>
             <div className="ui simple dropdown item">
-              Categories <i className="dropdown icon"></i>
+              Categories <i className="dropdown icon" />
               <Menu secondary id="navbar">
-                <Menu.Item name='environment' active={activeItem === 'home'} onClick={this.handleItemClick} />
-                <Menu.Item name='medicine' active={activeItem === 'home'} onClick={this.handleItemClick} />
-                <Menu.Item name='outreach' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                <Menu.Item
+                  name="environment"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="medicine"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="outreach"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                />
               </Menu>
             </div>
-            <Menu.Menu position='right'>
-              <Menu.Item><Input icon='search' placeholder='Search...' /></Menu.Item>
+            <div className="ui simple dropdown item">
+              Language <i className="dropdown icon" />
+              <Menu secondary id="language">
+                <Menu.Item
+                  name="Swedish"
+                  // <i> PUT IN A FLAG!!!
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="English"
+                  onClick={this.handleItemClick}
+                />
+              </Menu>
+            </div>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <Input icon="search" placeholder="Search..." />
+              </Menu.Item>
               {createArticleButton}
 
               {loginActions}
             </Menu.Menu>
           </Container>
         </div>
-        <Container>
-          {flashMessage}
-        </Container>
+        <Container>{flashMessage}</Container>
       </>
-    )
+    );
   }
 }
 
