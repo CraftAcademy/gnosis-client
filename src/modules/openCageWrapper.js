16718067547d0bb9a1604737a6160e9590617cd4
Thomas <thomas@craftacademy.se>
2019-08-24 11:48:52 +0200

    rafactors naming in open cage wrapper

--- src/modules/openCageWrapper.js
+++ src/modules/openCageWrapper.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
 const getAddress = async (lat, long) => {
-  var apikey = process.env.REACT_APP_OPEN_CAGE_API_KEY;
+  var apiKey = process.env.REACT_APP_OPEN_CAGE_API_KEY;
   var latitude = lat;
   var longitude = long;
 
-  var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+  var apiUrl = 'https://api.opencagedata.com/geocode/v1/json'
 
-  var request_url = api_url
+  var requestUrl = apiUrl
     + '?'
-    + 'key=' + apikey
+    + 'key=' + apiKey
     + '&q=' + encodeURIComponent(latitude + ',' + longitude)
     + '&pretty=1'
     + '&no_annotations=1';
 
-  let results = await axios.get(request_url)
+  let results = await axios.get(requestUrl)
   return results.data.results[0]
 }
 
