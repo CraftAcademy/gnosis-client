0dee4c8b2aa4c4683e6da7386dd69deb36c86c42
Seth NeJame <sethnejame@gmail.com>
2019-08-25 23:07:19 +0200

    Adding section in DisplayArticles for articles by location

--- src/components/DisplayArticles.js
+++ src/components/DisplayArticles.js
@@ -3,12 +3,10 @@ import axios from "axios";
 import ArticleTemplate from "./ArticleTemplate";
 import { Container } from "semantic-ui-react";
 
-
 class DisplayArticles extends Component {
   state = {
-      articles: []
-    };
-
+    articles: []
+  };
 
   componentDidMount() {
     this.getArticles();
@@ -16,7 +14,7 @@ class DisplayArticles extends Component {
 
   async getArticles() {
     const response = await axios.get("/articles");
-    if (response.data.length > 0) { 
+    if (response.data.length > 0) {
       this.setState({
         articles: response.data
       });
@@ -24,23 +22,29 @@ class DisplayArticles extends Component {
   }
 
   render() {
-
-    const articlesDisplay = this.state.articles.length > 0 && this.state.articles.map(article => {
-      return <ArticleTemplate key={article.id} article={article} />
-    })
+    const articlesDisplay =
+      this.state.articles.length > 0 &&
+      this.state.articles.map(article => {
+        return <ArticleTemplate key={article.id} article={article} />;
+      });
 
     return (
       <>
         <Container>
-          {articlesDisplay ?
-            articlesDisplay :
+          <p>
+            <h3>Local Research in your Area</h3>
+            {/* Insert code to match article city against user geotag here */}
+          </p>
+          {articlesDisplay ? (
+            articlesDisplay
+          ) : (
             <div className="articles">
               <p>Articles are currently unavailable.</p>
             </div>
-          }
+          )}
         </Container>
       </>
-    )
+    );
   }
 }
 
