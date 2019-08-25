410535b5d4987766122a2f94965a620f527c9cb8
Seth NeJame <sethnejame@gmail.com>
2019-08-25 20:15:08 +0200

    Ponging to Erik w/ mockGeolocation in action. . .still busted

--- cypress/support/stubLocation.js
+++ cypress/support/stubLocation.js
@@ -1,13 +1,13 @@
-cy.fixture('location.json').as('fakeLocation');
-cy.get('@fakeLocation').then(fakeLocation => {
-  cy
-    .visit('some-url', {
-      onBeforeLoad (win) {
-	cy
-	  .stub(win.navigator.geolocation, 'getCurrentPosition')
-	  .callsFake((cb) => {
-             return cb(fakeLocation);
-	  });
-      },
-  });
-});
+// cy.fixture('location.json').as('fakeLocation');
+// cy.get('@fakeLocation').then(fakeLocation => {
+//   cy
+//     .visit('some-url', {
+//       onBeforeLoad (win) {
+// 	cy
+// 	  .stub(win.navigator.geolocation, 'getCurrentPosition')
+// 	  .callsFake((cb) => {
+//              return cb(fakeLocation);
+// 	  });
+//       },
+//   });
+// });
