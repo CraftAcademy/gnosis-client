d5602060cdc365e12dff0e90cb518fb2fdeab65b
Seth NeJame <sethnejame@gmail.com>
2019-08-25 20:59:23 +0200

    Exporting geolocated from App w/ accuracy disabled

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import CreateArticleForm from "./components/CreateArticleForm";
 import LoginForm from "./components/LoginForm";
 import NavBar from "./components/NavBar.js";
 import "./styling/Navbar.css";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import SignupForm from "./components/SignupForm";
 import Homepage from "./components/Homepage";
-import PaymentForm from './components/PaymentForm';
-import { geolocated } from 'react-geolocated';
-
-
-
+import PaymentForm from "./components/PaymentForm";
+import { geolocated } from "react-geolocated";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/login-form" component={LoginForm} />
-          <Route exact path="/signup" component={SignupForm} />
-          <Route exact path="/createarticle" component={CreateArticleForm} />
-          <Route exact path="/payment" component={PaymentForm} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/login-form" component={LoginForm} />
+        <Route exact path="/signup" component={SignupForm} />
+        <Route exact path="/createarticle" component={CreateArticleForm} />
+        <Route exact path="/payment" component={PaymentForm} />
+      </Switch>
     </div>
   );
-}
+};
 
-export default App;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 5000
+})(App);
