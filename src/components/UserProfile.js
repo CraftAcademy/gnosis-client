6c87d9c065dc0eb17c9bc4677c675436b3edcdc6
pedro_bras <pedro.c.bras@gmail.com>
2019-08-28 00:31:00 +0200

    adds function to extract all elements from key array received by api, sorts all keys individualy for independent management

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Container } from "semantic-ui-react";
-import axios from 'axios'
+import axios from "axios";
 
 class UserProfile extends Component {
   state = {
-    regKeys: ""
+    regKey1: "",
+    regKey2: "",
+    regKey3: "",
+    regKey4: "",
+    regKey5: ""
   };
 
   componentDidMount() {
@@ -17,27 +21,50 @@ class UserProfile extends Component {
       "/subscriptions",
       this.props.currentUser.attributes.uid
     );
-    console.log(response);
-    debugger
     if (response) {
+      let ourArray = response.data;
+      function toArray(ourArray) {
+        const result = [];
+        for (const prop in ourArray) {
+          const value = ourArray[prop];
+          if (typeof value === "object") {
+            result.push(toArray(value));
+          } else {
+            result.push(value);
+          }
+        }
+        return result;
+      }
+      let allKeys = toArray(ourArray);
+
       this.setState({
-        regKeys: response.data.keys[0]
+        regKey1: allKeys[0],
+        regKey2: allKeys[1],
+        regKey3: allKeys[2],
+        regKey4: allKeys[3],
+        regKey5: allKeys[4]
       });
     }
   }
   render() {
     let profileContent;
 
-    if (this.props.currentUser.attributes.role === "university" &&
-    this.props.currentUser.attributes.subscriber === true) {
+    if (
+      this.props.currentUser.attributes.role === "university" &&
+      this.props.currentUser.attributes.subscriber === true
+    ) {
       profileContent = (
         <div>
           <h1>Registration Keys:</h1>
-          <div>{this.state.regKeys}</div>
+          <div>{this.state.regKey1}</div>
+          <div>{this.state.regKey2}</div>
+          <div>{this.state.regKey3}</div>
+          <div>{this.state.regKey4}</div>
+          <div>{this.state.regKey5}</div>
         </div>
       );
     }
-       
+
     return (
       <Container>
         <h1>Your Profile</h1>
@@ -47,13 +74,10 @@ class UserProfile extends Component {
   }
 }
 
-
 const mapStateToProps = state => {
   return {
     currentUser: state.reduxTokenAuth.currentUser
   };
 };
 
-export default connect(
-  mapStateToProps
-)(UserProfile);
+export default connect(mapStateToProps)(UserProfile);
