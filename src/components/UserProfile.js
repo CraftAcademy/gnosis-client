90b494c7ae407f2c0b0d0316c0cac7d807fa4395
pedro_bras <pedro.c.bras@gmail.com>
2019-08-27 23:40:27 +0200

    adds axios call to get keys from api, adds if statement to display keys when uni is logged in and subscribed

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Container } from "semantic-ui-react";
+import axios from 'axios'
 
 class UserProfile extends Component {
   state = {
-    name: '',
-    email: '',
-    role: '',
+    regKeys: ""
+  };
+
+  componentDidMount() {
+    this.getUniKeys();
   }
 
-  render () {
+  async getUniKeys() {
+    const response = await axios.get(
+      "/subscriptions",
+      this.props.currentUser.attributes.uid
+    );
+    console.log(response);
+    debugger
+    if (response) {
+      this.setState({
+        regKeys: response.data.keys[0]
+      });
+    }
+  }
+  render() {
+    let profileContent;
+
+    if (this.props.currentUser.attributes.role === "university" &&
+    this.props.currentUser.attributes.subscriber === true) {
+      profileContent = (
+        <div>
+          <h1>Registration Keys:</h1>
+          <div>{this.state.regKeys}</div>
+        </div>
+      );
+    }
+       
     return (
       <Container>
         <h1>Your Profile</h1>
+        {profileContent}
       </Container>
     );
   }
