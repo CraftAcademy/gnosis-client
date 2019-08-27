35c5f5e12832ad0879b934f7dca3904dbc07e48c
pedro_bras <pedro.c.bras@gmail.com>
2019-08-27 23:39:37 +0200

    adds keys as array in fixture file and adds should contain keys in feature test

--- cypress/integration/universityCanSeeRegistrationKeys.spec.js
+++ cypress/integration/universityCanSeeRegistrationKeys.spec.js
@@ -12,9 +12,21 @@ describe("University pay for subscription", () => {
   });
 
   it("University can access profile page and see registration keys", () => {
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v0/subscriptions",
+      response: "fixture:university_registration_keys.json",
+      status: 200
+    });
     cy.get("#subscribe-button").should("not.exist");
     cy.get("#profile-button").click();
     cy.contains("Your Profile");
+    cy.contains("Registration Keys:");
+    cy.contains("b3tDSWucTHtzWLNNGFdgagip");
+    cy.contains("U9YT35JejpfKDy1vfuWTVLyb");
+    cy.contains("dWCWyfnNn4DVFEHzAAz7b6pw");
+    cy.contains("6evmpvxxJmHxvWdNZzZYKa4i");
+    cy.contains("fGygSg6Fxjav78tKEbLzS3TB");
   })
 
 })
