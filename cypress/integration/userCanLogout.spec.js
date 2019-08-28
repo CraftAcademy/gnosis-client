ba5c5d7e3bc9bcc8c80cc168e987657d954c09a2
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-28 10:25:16 +0200

    adds test to check that research group ca successfully log out and removes test to GET articles

--- cypress/integration/userCanLogout.spec.js
+++ cypress/integration/userCanLogout.spec.js
@@ -1,21 +1,30 @@
 describe('User Log-Out', () => {
    beforeEach(() => {
     cy.server();
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/v0/articles",
-      response: "fixture:articles.json"
-    })
   });
 
-  it("Can log out successfully", () => {
+  it("University can log out successfully", () => {
     cy.route({
       method: 'DELETE',
       url: 'http://localhost:3000/api/v0/auth/sign_out',
       response: 'fixture:successful_user_logout.json',
     })
     cy.visit("http://localhost:3001")
-    cy.university_login("harvard@mail.com", "password");
+    cy.university_login("harvard@harvard.edu", "password");
+    cy.get("#logout-button").should("exist");
+    cy.get("#logout-button").click();
+    cy.get("#flash").should("contain",  "You have successfully logged out.")
+    cy.get("#login-button").should("exist");
+  });
+
+  it("Reseach Group can log out successfully", () => {
+    cy.route({
+      method: 'DELETE',
+      url: 'http://localhost:3000/api/v0/auth/sign_out',
+      response: 'fixture:successful_user_logout.json',
+    })
+    cy.visit("http://localhost:3001")
+    cy.research_group_login("climate_research@harvard.edu", "password");
     cy.get("#logout-button").should("exist");
     cy.get("#logout-button").click();
     cy.get("#flash").should("contain",  "You have successfully logged out.")
