337d08b126b4951fa70fb0ee25aa4ff33116bfde
oliverochman <oliver.ochman@hotmail.com>
2019-08-28 12:12:23 +0200

    removes locationReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import { reduxTokenAuthReducer } from "redux-token-auth";
 import flashMessageReducer from './flashMessageReducer'
-import locationReducer from "./locationReducer";
 
 const rootReducer = combineReducers({
   reduxTokenAuth: reduxTokenAuthReducer,
-  flashes: flashMessageReducer,
-  city: locationReducer
+  flashes: flashMessageReducer
 });
 
 export default rootReducer;
