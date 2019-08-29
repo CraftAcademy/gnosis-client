1697f86c382ce258913ff9a26d0c5008dddc7887
erka <erik.westerberg@hotmail.se>
2019-08-29 08:31:55 +0200

    also brought in an flashmessage if user not sharing location

--- src/components/DisplayArticles.js
+++ src/components/DisplayArticles.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import ArticleTemplate from "./ArticleTemplate";
 import { Container } from "semantic-ui-react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import getCity from "../modules/openCageWrapper";
 
 class DisplayArticles extends Component {
   state = {
     articles: [],
-    position: {},   
+    position: {},
     city: "",
     localArticles: []
   };
@@ -31,18 +31,18 @@ class DisplayArticles extends Component {
       if (this.state.city) {
         this.getCityArticles();
       }
-    }); 
+    });
   }
 
   async getCityArticles() {
     try {
-      const response = await axios.get(
-        `/articles/${this.state.city}`
-      );
+      const response = await axios.get(`/articles/${this.state.city}`);
       this.setState({ localArticles: response.data });
     } catch (error) {
       this.props.dispatchFlash(
-        "If you want to enable local Research section, please share your location","error")
+        "If you want to enable local Research section, please share your location",
+        "error"
+      );
     }
   }
 
@@ -56,7 +56,7 @@ class DisplayArticles extends Component {
   }
 
   render() {
-    let articleCityDisplay; 
+    let articleCityDisplay;
     const articlesDisplay =
       this.state.articles.length > 0 &&
       this.state.articles.map(article => {
@@ -69,9 +69,9 @@ class DisplayArticles extends Component {
           <h1>Local Research</h1>
           {this.state.localArticles.map(article => {
             return <ArticleTemplate key={article.id} article={article} />;
-          })} 
+          })}
         </div>
-      )   
+      );
     }
 
     return (
@@ -98,7 +98,10 @@ const mapDispatchToProps = {
   dispatchFlash: (message, status) => ({
     type: "SHOW_FLASH_MESSAGE",
     payload: { flashMessage: message, status: status }
-  }),
+  })
 };
 
-export default connect(null, mapDispatchToProps)(DisplayArticles);
+export default connect(
+  null,
+  mapDispatchToProps
+)(DisplayArticles);
