c1465bddb83467c7549846b2314c30b8790d7758
erka <erik.westerberg@hotmail.se>
2019-08-29 16:14:21 +0200

    refactores test, green for wrong reason

--- cypress/integration/visitorSeesLocationSpecificArticle.spec.js
+++ cypress/integration/visitorSeesLocationSpecificArticle.spec.js
@@ -9,7 +9,7 @@ describe("Visitor can see articles when visiting the App", () => {
     });
     cy.route({
       method: "GET",
-      url: "http://localhost:3000/api/v0/articles?**",
+      url: "http://localhost:3000/api/v0/articles/Stockholm",
       response: "fixture:local_articles.json"
     });
     cy.visit(
@@ -18,18 +18,11 @@ describe("Visitor can see articles when visiting the App", () => {
     );
   });
 
-  it("Two articles are displayed", async () => {
-    cy.get("#article_1");
-    cy.get("#article_2");
-  });
-
   it("Article is from Stockholm", async () => {
     cy.get("#local-research").within(() => {
       cy.get("#article_1").within(() => {
         cy.get("#city").contains("Stockholm");
-        cy.get("#article_2").within(() => {
-          cy.get("#city").contains("Stockholm");
-        });
+       ;
       });
     });
   });
